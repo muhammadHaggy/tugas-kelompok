41c463ea3019731ac0261d32ab2cb9d42538ea93
Muhammad Haggy <86955979+muhammadHaggy@users.noreply.github.com>
2022-10-27 16:39:31 +0700

    change onetoonefield to foreign key unique

--- user/models.py
+++ user/models.py
@@ -1,13 +1,15 @@
+from enum import unique
 from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
 class UserDetails(models.Model):
-    user = models.OneToOneField(
+    user = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
         primary_key=True,
         editable=False,
+        unique=True,
     )
     tanggal_lahir = models.DateField()
     bio_singkat = models.TextField()
