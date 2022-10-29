a1ce472f44ee1fe38e3e4bf11bcf1060a141e26b
Febrian Dwi Kimhan <febrian.dwi11@ui.ac.id>
2022-10-30 01:18:50 +0700

    bikin page galang dana

--- galang_dana/urls.py
+++ galang_dana/urls.py
@@ -1,3 +1,4 @@
+from unicodedata import name
 from django.urls import path
 from galang_dana.views import *
 
@@ -5,4 +6,6 @@ app_name = 'galang_dana'
 
 urlpatterns = [
     path('', show_galang_dana, name='show_galang_dana'),
+    path('create/', show_buat_galang_dana, name='show_buat_galang_dana'),
+    path('get-user-count/', get_user_count_json, name='get_user_count'),
 ]
--- galang_dana/views.py
+++ galang_dana/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from user.models import UserDetails
+
+from django.http import HttpResponse, HttpResponseNotFound
+from django.core import serializers
+
 
 # Create your views here.
 def show_galang_dana(request):
     return render(request, 'galang-dana.html')
+
+def show_buat_galang_dana(request):
+    return render(request, 'buat-galang-dana.html')
+
+def get_user_count_json(request):
+    all_donatur = UserDetails.objects.all()
+    return HttpResponse(serializers.serialize("json", all_donatur))
