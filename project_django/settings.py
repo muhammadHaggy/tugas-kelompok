243eed9cadffff4106a5b88aa258e0aaea56b57e
mhadziq0403 <mhadziq0403@gmail.com>
2022-11-28 11:17:56 +0700

    databse to postgres

--- project_django/settings.py
+++ project_django/settings.py
@@ -85,10 +85,21 @@ WSGI_APPLICATION = 'project_django.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'aGN00p2aexMepbgAgqpE',
+        'HOST': 'containers-us-west-125.railway.app',
+        'PORT': '5932',
     }
 }
 
