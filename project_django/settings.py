39a0db663b4e3919cc1cae7bc41581094b992bfe
mhadziq0403 <mhadziq0403@gmail.com>
2022-11-28 12:02:10 +0700

    revert database to sql3

--- project_django/settings.py
+++ project_django/settings.py
@@ -85,23 +85,23 @@ WSGI_APPLICATION = 'project_django.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'aGN00p2aexMepbgAgqpE',
-        'HOST': 'containers-us-west-125.railway.app',
-        'PORT': '5932',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'railway',
+#         'USER': 'postgres',
+#         'PASSWORD': 'aGN00p2aexMepbgAgqpE',
+#         'HOST': 'containers-us-west-125.railway.app',
+#         'PORT': '5932',
+#     }
+# }
 
 
 
