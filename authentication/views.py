d75f6cd3a2791dc47d45d492c474dfd7c1083ad6
Muhammad Haggy <86955979+muhammadHaggy@users.noreply.github.com>
2022-11-30 18:17:27 +0700

    make register json auto logging in user

--- authentication/views.py
+++ authentication/views.py
@@ -7,37 +7,42 @@ from django.contrib.auth.forms import UserCreationForm
 from user.models import UserDetails
 
 
+def login_logic(request, username, password):
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        if user.is_active:
+            auth_login(request, user)
+            # Redirect to a success page.
+            return JsonResponse({
+                "status": True,
+                "message": "Successfully Logged In!"
+                # Insert any extra data if you want to pass data to Flutter
+            }, status=200)
+        else:
+            return JsonResponse({
+                "status": False,
+                "message": "Failed to Login, Account Disabled."
+            }, status=401)
+
+    else:
+        return JsonResponse({
+            "status": False,
+            "message": "Failed to Login, check your email/password."
+        }, status=401)
+
+
 @csrf_exempt
 def login(request):
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            if user.is_active:
-                auth_login(request, user)
-                # Redirect to a success page.
-                return JsonResponse({
-                    "status": True,
-                    "message": "Successfully Logged In!"
-                    # Insert any extra data if you want to pass data to Flutter
-                }, status=200)
-            else:
-                return JsonResponse({
-                    "status": False,
-                    "message": "Failed to Login, Account Disabled."
-                }, status=401)
-
-        else:
-            return JsonResponse({
-                "status": False,
-                "message": "Failed to Login, check your email/password."
-            }, status=401)
+        return login_logic(request, username, password)
     else:
         return JsonResponse({
             "status": False,
             "message": "Use POST request!"
         })
+
 
 @csrf_exempt
 def register(request):
@@ -48,7 +53,7 @@ def register(request):
             user.first_name = user.username
             user.save()
             UserDetails(user=user).save()
-            return JsonResponse({"status": True, "message": "Account successfuly created!"})
+            return login_logic(request, user.username, request.POST['password1'])
         else:
             return JsonResponse({"status": False, "message": form.errors.as_text()})
 
