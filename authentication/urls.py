0b2ebd8f05d1f7dea8b24ac2dfa4db2b699d79c8
Muhammad Haggy <86955979+muhammadHaggy@users.noreply.github.com>
2022-11-30 18:26:11 +0700

    add register json to url

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import login
+from .views import login, register
 
 app_name = 'authentication'
 
 urlpatterns = [
     path('login', login, name="login"),
+    path('register', register, name='register'),
 ]
