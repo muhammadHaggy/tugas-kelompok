ce2d4853e9989399b0bc8f03e5fdfc61cb5e80e8
maritzarahayu <maritzarahayui@gmail.com>
2022-12-12 13:55:44 +0700

    edit

--- canwe/models.py
+++ canwe/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Contact(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
+    # user = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
     name = models.CharField(max_length=200, default="")
     email = models.EmailField(max_length=200, default="")
     question = models.TextField(max_length=2000, default="")
